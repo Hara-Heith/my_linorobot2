9f17b2b29266f340af4a1a7e3e74cd114b9ddd49
Heith Hara <mrphucdu2002@gmail.com>
2025-12-19 15:15:03 +0700

    Custom humble setup for my robot

--- linorobot2_description/launch/description.launch.py
+++ linorobot2_description/launch/description.launch.py
@@ -29,7 +29,7 @@ def generate_launch_description():
     )
 
     rviz_config_path = PathJoinSubstitution(
-        [FindPackageShare('linorobot2_description'), 'rviz', 'description.rviz']
+        [FindPackageShare('linorobot2_description'), 'rviz', 'description_mine.rviz']
     )
 
     return LaunchDescription([
